d49202e5ad040317edaa78a8f0f10cdf4bbad683
Daniel Acquesta <danielacquesta@sbcglobal.net>
2017-08-24 12:50:22 -0700

    fully functioning CRUD endpoints

---
+++ models.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const blogSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  author: {
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true}
+  },
+  created: {type: Date, required: true}
+});
+
+
+blogSchema.virtual('authorString').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim()});
+
+
+blogSchema.methods.apiRepr = function() {
+
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.authorString,
+    created: this.created
+  };
+};
+
+// note that all instance methods and virtual properties on our
+// schema must be defined *before* we make the call to `.model`.
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = {Blog};
